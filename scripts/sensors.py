f09a2e3be561544d71db6b97ba19200af979b9bf
Elkholy2021 <hassanashrafelkholy2020@gmail.com>
2022-11-24 16:39:26 +0100

    imu sub + eul2quat

--- scripts/sensors.py
+++ scripts/sensors.py
@@ -1,31 +1,31 @@
 #!/usr/bin/env python
 import rospy
+import euler2quat as e2q
 from std_msgs.msg import Bool
 from mavros_msgs.msg import StatusText
-def chatter_callback(message):
+from sensor_msgs.msg import Imu
+def status_callback(message):
     #get_caller_id(): Get fully resolved name of local node
     pub = rospy.Publisher('leak', Bool, queue_size=10)
     if "Leak" in message.text:
-
         rospy.loginfo(rospy.get_caller_id() + "there is a leak with severity =  ", message.severity)
         status = True
     else:
         rospy.loginfo(rospy.get_caller_id() + "No leak. Go Fish Captain!")
         status = False
     pub.publish(status)
-    
-def listener():
+def imu_callback(message):
+    q = message.orientation
+    [roll, pitch, yaw ] = e2q.euler_from_quaternion(q)
+    print(roll,pitch,yaw)
+def sensors():
+    rospy.init_node('sensors', anonymous=True)
 
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # node are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-    rospy.init_node('listener', anonymous=True)
+    rospy.Subscriber("/mavros/statustext/recv", StatusText, status_callback)
+    rospy.Subscriber("/mavros/imu/data", Imu, imu_callback)
 
-    rospy.Subscriber("/mavros/statustext/recv", StatusText, chatter_callback)
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
 if __name__ == '__main__':
-    listener()
+    sensors()
