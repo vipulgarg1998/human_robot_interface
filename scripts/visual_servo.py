d5967bee3d15800e8a2cdd034312538173bf6223
vipulgarg1998 <garg.vipul7@gmail.com>
2022-12-15 17:22:20 +0100

    aruco added

--- scripts/visual_servo.py
+++ scripts/visual_servo.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 import rospy
+import numpy as np
 from std_msgs.msg import Bool
 from std_msgs.msg import Float64
 from sensor_msgs.msg import Image
 import cv2
+from cv2 import aruco
 from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
 
 
@@ -29,13 +31,35 @@ class VisualServo:
         self.cv_image = self.cv_bridge.imgmsg_to_cv2(image_msg)
         self.width = self.cv_image.shape[1]
         self.height = self.cv_image.shape[0]
+        self.detectMarkers(self.cv_image)
         # Convert from BGR to RGB color format.
-        cv2.imshow('image', self.cv_image)
-        #   out.write(nimg)
-        # Press `q` to exit.
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            self.exit()
+        # cv2.imshow('image', self.cv_image)
+        # #   out.write(nimg)
+        # # Press `q` to exit.
+        # if cv2.waitKey(1) & 0xFF == ord('q'):
+        #     self.exit()
 
+    def find_centroid(self, corners):
+        for corner in corners:
+            x = np.mean(corner[0, :, 0])
+            y = np.mean(corner[0, :, 1])
+            print(f'X is {x} and Y is {y}')
+    
+    def detectMarkers(self, image, view=True):
+        gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+        aruco_dict = aruco.Dictionary_get(aruco.DICT_ARUCO_ORIGINAL)
+        parameters = aruco.DetectorParameters_create()
+        # print("parameters", parameters)
+        corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters = parameters)
+        corners = np.array(corners)
+        print("corners",corners.shape)
+        self.find_centroid(corners)
+        # print("ids", ids)
+        # print("rejectedImgPoints",rejectedImgPoints)
+        if(view):
+            frame_markers = aruco.drawDetectedMarkers(image.copy(), corners, ids)
+            cv2.imshow('aruco_image', frame_markers)
+            cv2.waitKey(1)
         
 def main(args=None):
     # initializing the subscriber node
